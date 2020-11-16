03502af4e9c71d80f053a7574107285b6a141ec3
jywu98 <60245438+jywu98@users.noreply.github.com>
2020-11-16 16:46:14 +0800

    Update Index.java

--- Index.java
+++ Index.java
@@ -3,14 +3,20 @@ package project;
 import java.util.ArrayList;
 
 public class Index {
+	protected Course course;
 	protected int index;
 	protected int classLimit;
 	protected ArrayList<Lesson> indexLessons;
+	protected ArrayList<StudentAccount> registeredStudents;
+	protected ArrayList<StudentAccount> waitlist;
 	
-	public Index(int index, int classLimit) {
+	public Index(Course course, int index, int classLimit) {
+		this.course = course;
 		this.index = index;
 		this.classLimit = classLimit;
 		indexLessons = new ArrayList<Lesson>();
+		registeredStudents = new ArrayList<StudentAccount>();
+		waitlist = new ArrayList<StudentAccount>();
 	}
 	
 	public int getIndex() {
@@ -36,4 +42,34 @@ public class Index {
 	public void addLesson(Lesson lesson){
 		indexLessons.add(lesson);
 	}	
+	
+	public int getVacancies(){
+		return classLimit - registeredStudents.size();
+	}
+	
+	public void registerStudent(StudentAccount student){
+		registeredStudents.add(student);
+	}
+	
+	public void dropStudent(StudentAccount student){
+		registeredStudents.remove(student);
+		if (waitlist.size() > 0){
+			StudentAccount newstudent = waitlist.get(0);
+			registeredStudents.add(newstudent);
+			waitlist.drop(newstudent);
+			/* send email notif here */
+		}
+	}
+	
+	public void waitStudent(StudentAccount student){
+		waitlist.add(student);
+	}
+	
+	public void unwaitStudent(StudentAccount student){
+		waitlist.remove(student);
+	}
+	
+	public int getWaitlistLength(){
+		return waitlist.size();
+	}
 }
