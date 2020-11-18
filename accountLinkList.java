544b35e304b435474ba303265213775bc91c68cc
shaunong98 <74414749+shaunong98@users.noreply.github.com>
2020-11-18 23:08:59 +0800

    Update accountLinkList.java

--- accountLinkList.java
+++ accountLinkList.java
@@ -1,16 +1,45 @@
 package teamassignment;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class accountLinkList {
 	
-	ArrayList<UserAccount> accounts = new ArrayList<UserAccount>();
+	private ArrayList<UserAccount> userlist;
+	
+	public accountLinkList() {
+		this.userlist = new ArrayList<UserAccount>();
+	}
+	
+	public void createUserlist() throws Exception 
+		{
+	        try
+	        {
+	            FileInputStream fis = new FileInputStream("userData");
+	            ObjectInputStream ois = new ObjectInputStream(fis);
+	 
+	            userlist = (ArrayList) ois.readObject();
+	 
+	            ois.close();
+	            fis.close();
+	            
+	        } 
+	        catch (IOException e) 
+	        {
+	            System.out.println(e.getMessage());
+	        } 
+		}
+	public ArrayList<UserAccount> getUserlist(){
+		return userlist;
+	}
 	
 	public void addAccount(UserAccount user) {
-		accounts.add(user);
+		userlist.add(user);
 	}
 	
 	public void removeAccount(UserAccount user) {
-		accounts.remove(user);
+		userlist.remove(user);
 	}
 }
