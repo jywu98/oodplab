ae0f71e6ce902200bc6a891658e1326cb192649b
jywu98 <60245438+jywu98@users.noreply.github.com>
2020-11-19 22:05:55 +0800

    Update Schedule.java

--- Schedule.java
+++ Schedule.java
@@ -2,20 +2,24 @@ import java.util.ArrayList;
 
 public class Schedule {
     protected String matricnumber;
+    protected ArrayList<Index> registered;
     protected ArrayList<Lesson> timetable;
 
     public Schedule(String matricnumber){
         this.matricnumber = matricnumber;
+        registered = new ArrayList<Index>();
         timetable = new ArrayList<Lesson>();
     }
 
-    public void addCourse(ArrayList<Lesson> lessons){
+    public void addCourse(Index index_){
+        registered.add(index_);
+        ArrayList<Lesson> lessons = index_.getLessons();
         for (int i = 0; i < lessons.size(); i++){
             Lesson selected = lessons.get(i);
             for (int j = 0; j < timetable.size(); j++){
                 if (checkClash(selected, timetable.get(j)) == false){
                     System.out.println("Error - there is a clash");
-                    return;
+                    return; /*maybe return false here?*/
                 }
             }
         }
@@ -24,7 +28,9 @@ public class Schedule {
         }
     }
 
-    public void dropCourse(ArrayList<Lesson> lessons){
+    public void dropCourse(Index index_){
+        registered.remove(index_);
+        ArrayList<Lesson> lessons = index_.getLessons();
         for (int i = 0; i < lessons.size(); i++){
             this.timetable.remove(lessons.get(i));
         }
