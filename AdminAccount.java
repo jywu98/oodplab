f700685d9e28ef305dcbb478686e26cc3637ac5d
simhaoong <74414363+simhaoong@users.noreply.github.com>
2020-11-20 20:17:39 +0800

    Update AdminAccount.java

--- AdminAccount.java
+++ AdminAccount.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class AdminAccount extends UserAccount{
 
-	public AdminAccount(String username, String password, boolean admin, String name, String gender, String nationality, String email) {
-		super(username, password, admin, name, gender, nationality, email);
+	public AdminAccount(String username, String password, boolean admin, String gender, String nationality, String email) {
+		super(username, password, admin, gender, nationality, email);
 	}
 	
 	public void editAccessPeriod() {
@@ -20,9 +20,6 @@ public class AdminAccount extends UserAccount{
 	public void addStudent(String username, String password, boolean admin, String gender, String nationality, String email, int auLimit, String matricnumber) {
 		StudentAccount newstudent = new StudentAccount(username, hashtool.getHash(password), admin, gender, nationality, email, auLimit, matricnumber);
 		UserList.addAccount(newstudent);
-		
-		
-		
 	}
 	
 	public void addCourse(String coursecode, String courseName, int au) {
@@ -31,20 +28,34 @@ public class AdminAccount extends UserAccount{
 	}
 	
 	
-	public void updateCourseIndex(courseLinkList mycourselist, int oldindex, int newindex) {
+	public void updateCourseIndex(courseLinkList mycourselist, String mycoursecode, int oldindex, int newindex) {
 		
-		for (int i = 0; i< courseLinkList.length(); i++)
+		for (int i = 0; i< courseLinkList.size(); i++)
 		{			
-			if (courseLinkList.get(i).get == oldindex) //mycourse-> get arraylist of index-> get specific index-> get index name of index ==oldindex
+			if (courseLinkList.get(i).getCourseCode() == mycoursecode) 
 			{
-				mycourse.getIndex().get(i).setIndex(newindex); //mycourse->get arraylist of index-> get specific index-> set index name of index
+				Course mycourse = courseLinkList.get(i);
 			}
 		}
 		
+		for (int j = 0; j< mycourse.getIndex.size(); j++)
+		{
+			if (mycourse.getIndex.get(j).getIndex() == oldindex)
+			{
+				mycourse.getIndex.get(j).setIndex() == newindex;	
+			}
+		}
 	}
 	
-	public void updateCourseCode(Course mycourse, String newcoursecode) {
-		mycourse.setCourseCode(newcoursecode);
+	public void updateCourseCode(courseLinkList mycourselist, String oldcoursecode, String newcoursecode) {
+		
+		for (int i = 0; i< courseLinkList.size(); i++)
+		{			
+			if (courseLinkList.get(i).getCourseCode() == oldcoursecode) 
+			{
+				courseLinkList.get(i).setCourseCode(newcoursecode);
+			}
+		}
 			
 	}
 	
