5eae15366dde9e154fd4b8224ab915ab3b725bd9
simhaoong <74414363+simhaoong@users.noreply.github.com>
2020-11-20 19:44:16 +0800

    Update courseLinkList.java

--- courseLinkList.java
+++ courseLinkList.java
@@ -1,1 +1,45 @@
+package teamassignment;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+public class accountLinkList {
+	
+	private ArrayList<UserAccount> userlist;
+	
+	public accountLinkList() {
+		this.courseList = new ArrayList<Course>();
+	}
+	
+	public void createCourselist() throws Exception 
+		{
+	        try
+	        {
+	            FileInputStream fis = new FileInputStream("courseData");
+	            ObjectInputStream ois = new ObjectInputStream(fis);
+	 
+	            courseList = (ArrayList) ois.readObject();
+	 
+	            ois.close();
+	            fis.close();
+	            
+	        } 
+	        catch (IOException e) 
+	        {
+	            System.out.println(e.getMessage());
+	        } 
+		}
+	public ArrayList<Course> getCourseList(){
+		return userlist;
+	}
+	
+	public void addCourse(Course mycourse) {
+		courseList.add(mycourse);
+	}
+	
+	public void removeCourse(Course mycourse) {
+		courseList.remove(mycourse);
+	}
+}
