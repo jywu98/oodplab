4553cd79a56ec6b249fb96de1ebf9395a71819fc
jywu98 <60245438+jywu98@users.noreply.github.com>
2020-11-20 20:06:27 +0800

    Update IndexMasterList.java

--- IndexMasterList.java
+++ IndexMasterList.java
@@ -1,3 +1,25 @@
-public class IndexMasterList{
-
+public class IndexMasterList {
+	
+	private ArrayList<Index> indexList;
+	
+	public IndexMasterList() {
+		this.indexList = new ArrayList<Index>();
+	}
+  
+	public Index getIndex(int index_){
+		for (int i = 0; i < indexList.size(); i++){
+			if (indexList.get(i).getIndex() == index_){
+				returnIndexList.get(i)
+			}
+		}
+		System.out.println("Error - invalid index!"); /*need exception handling here I think*/
+	}
+  
+	public void addIndex(Index new) {
+		indexList.add(new);
+	}
+	
+	public void removeIndex(Index index_) {
+		indexList.remove(index_);
+	}
 }
